beb338d666b2ad8eeab5853c22e4561f50d76c1a
M.Amir Shabbir <m.amirshabbir99@gmail.com>
2025-07-29 15:00:48 +0500

    name update and cookie

--- Server/utils/jwtToken.js
+++ Server/utils/jwtToken.js
@@ -2,6 +2,7 @@
 
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
+  const isProduction = process.env.NODE_ENV === "production";
   res
     .status(statusCode)
     .cookie("token", token, {
@@ -9,6 +10,8 @@ export const generateToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+         secure: isProduction, // true for HTTPS (production), false for HTTP (localhost)
+    sameSite: isProduction ? "None" : "Lax"
     })
     .json({
       success: true,
